c901e91091099dcc775d1689ec98350521ed747d
longshiba <trandanghienlong2004@gmail.com>
2025-03-08 23:58:44 +0700

    fix: fix Pos embed and Transformer Layers

--- detr_model.py
+++ detr_model.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 
 from torch import nn
@@ -7,13 +8,22 @@ import torch.nn.functional as F
 
 
 class DETR(nn.Module):
-    def __init__(self, backbone, transformer, num_classes, num_queries, hidden_dim):
+    def __init__(
+        self,
+        num_classes: int,
+        num_queries: int,
+        hidden_dim: int,
+        feedforward_dim: int = 2048,
+        num_heads: int = 8,
+        encoder_num: int = 6,
+        decoder_num: int = 6,
+        temperature: int = 10000,
+    ):
         super().__init__()
-        self.backbone = backbone
-        self.transformer = transformer
         self.num_queries = num_queries
         self.num_classes = num_classes
         self.hidden_dim = hidden_dim
+        self.temperature = temperature
 
         self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
         # 4 is the number of coords in a bounding box
@@ -23,47 +33,103 @@ class DETR(nn.Module):
             activation_layer=nn.ReLU,
         )
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
-        self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, 1)
 
         ###### Transformer Setting ######
 
-    def _add_pos_embed(self, x, temperature=10000):
+        self.encoder_layer = TransformerEncoderLayer(
+            hidden_dim, feedforward_dim, num_heads
+        )
+        self.decoder_layer = TransformerDecoderLayer(
+            hidden_dim, feedforward_dim, num_heads
+        )
+
+        self.encoder = TransformerEncoder(self.encoder_layer, encoder_num)
+        self.decoder = TransformerDecoder(
+            self.encoder_layer, decoder_num, nn.LayerNorm(hidden_dim)
+        )
+
+        ###### Loss Setting ######
+
+    def _pos_embed(self, x):
         # x = (batch_size, sequence_length, hidden_dim)
-        _, L, C = x.shape
+        _, H, W, C = x.shape
+        embed_dim = self.hidden_dim // 2
 
         # dim_t = [t^(2*0/hidden_dim), t^(2*1/hidden_dim), t^(2*2/hidden_dim), t^(2*3/hidden_dim)]
-        dim_t = torch.arange(0, C, dtype=torch.float32, device=x.device)
-        dim_t = temperature ** (2 * dim_t) / C
-        # pos = (sequence_length, hidden_dim)
-        # [
-        #     [1, 1, 1, 1]
-        #     [2, 2, 2, 2]
-        #     [3, 3, 3, 3]
-        #     [4, 4, 4, 4]
-        # ]
-        pos = (
-            torch.arange(1, L + 1, dtype=torch.float32, device=x.device)
-            .unsqueeze(1)
-            .repeat(1, C)
+        dim_t = torch.arange(0, embed_dim, dtype=torch.float32, device=x.device)
+        dim_t = torch.pow(
+            self.temperature,
+            2 * (dim_t // 2) / embed_dim,
         )
-        # Calculating the position embedding according to Transformer paper
-        pos /= dim_t.unsqueeze(0)
-        pos[:, 1::2] = torch.cos(pos[:, 1::2])
-        pos[:, 0::2] = torch.sin(pos[:, 0::2])
 
-        return x + pos.unsqueeze(0)
+        y_embed = torch.arange(1, H + 1, dtype=torch.float32, device=x.device)
+        x_embed = torch.arange(1, W + 1, dtype=torch.float32, device=x.device)
+        x_pos, y_pos = torch.meshgrid(y_embed, x_embed)
+
+        x_pos = x_pos[:, :, None] / dim_t
+        y_pos = y_pos[:, :, None] / dim_t
+        x_pos = torch.stack(
+            (x_pos[:, :, 0::2].sin(), x_pos[:, :, 1::2].cos()), dim=3
+        ).flatten(2)
+        y_pos = torch.stack(
+            (y_pos[:, :, 0::2].sin(), y_pos[:, :, 1::2].cos()), dim=3
+        ).flatten(2)
+
+        pos = torch.cat((x_pos, y_pos), dim=2).unsqueeze(0).permute(0, 3, 1, 2)
+
+        return pos
 
     def forward(self, x):
+        pos_embed = self._pos_embed(x)
 
-        pass
+        memory = self.encoder(x, pos_embed)
+        target = torch.zeros_like(self.query_embed)
+        output = self.decoder(target, memory, pos_embed, self.query_embed)
+
+        return output
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, out_channels, ffn_dim, num_heads, num_layers, dropout=0.1):
+    def __init__(self, encoder_layer, num_layers, norm=None):
+        super().__init__()
+        self.layers = _get_clones(encoder_layer, num_layers)
+        self.norm = norm
+
+    def forward(self, x, pos_embed):
+        output = x
+
+        for layer in self.layers:
+            output = layer(output, pos_embed)
+
+        if self.norm:
+            output = self.norm(output)
+
+        return output
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers, norm=None):
+        super().__init__()
+        self.layers = _get_clones(decoder_layer, num_layers)
+        self.norm = norm
+
+    def forward(self, x, memory, pos_embed, query_embed):
+        output = x
+
+        for layer in self.layers:
+            output = layer(output, memory, pos_embed, query_embed)
+
+        if self.norm:
+            output = self.norm(output)
+
+        return output
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, out_channels, ffn_dim, num_heads, dropout=0.1):
         super().__init__()
         self.out_channels = out_channels
         self.num_heads = num_heads
-        self.num_layers = num_layers
         self.dropout = dropout
 
         self.attn = nn.MultiheadAttention(out_channels, num_heads, dropout)
@@ -79,12 +145,16 @@ class TransformerEncoder(nn.Module):
         self.norm2 = nn.LayerNorm(out_channels)
         self.dropout2 = nn.Dropout(dropout)
 
-    def forward(self, x):
+    def forward(self, x, pos_embed):
+        # positional memoryedding at each encoding layer
+        q = k = x + pos_embed
+
         # self-attention
-        x_attn = self.attn(x, x, x)
+        x_attn = self.attn(q, k, x)
         x = x + self.dropout1(x_attn)
         x = self.norm1(x)
 
+        # feedforward
         x_ffn = self.ffn(x)
         x = x + self.dropout2(x_ffn)
         x = self.norm2(x)
@@ -92,12 +162,11 @@ class TransformerEncoder(nn.Module):
         return x
 
 
-class TransformerDecoder(nn.Module):
-    def __init__(self, out_channels, ffn_dim, num_heads, num_layers, dropout=0.1):
+class TransformerDecoderLayer(nn.Module):
+    def __init__(self, out_channels, ffn_dim, num_heads, dropout=0.1):
         super().__init__()
         self.out_channels = out_channels
         self.num_heads = num_heads
-        self.num_layers = num_layers
         self.dropout = dropout
 
         self.self_attn = nn.MultiheadAttention(out_channels, num_heads, dropout)
@@ -117,14 +186,19 @@ class TransformerDecoder(nn.Module):
         self.norm3 = nn.LayerNorm(out_channels)
         self.dropout3 = nn.Dropout(dropout)
 
-    def forward(self, x, emb):
+    def forward(self, x, memory, pos_embed, query_embed):
+        # positional embedding at each decoding layer
+        q_self = k_self = x + query_embed
+
         # self-attention
-        x_attn = self.self_attn(x, x, x)
-        x = x + self.dropout1(x_attn)
+        self_attn = self.self_attn(q_self, k_self, x)
+        x = x + self.dropout1(self_attn)
         x = self.norm1(x)
 
-        # cross-attention
-        cross_attn = self.cross_attn(x, emb, emb)
+        # cross-attention with box query and positional embedding
+        q_cross = x + query_embed
+        k_cross = memory + pos_embed
+        cross_attn = self.cross_attn(q_cross, k_cross, memory)
         x = x + self.dropout2(cross_attn)
         x = self.norm2(x)
 
@@ -135,6 +209,16 @@ class TransformerDecoder(nn.Module):
         return x
 
 
+def _get_clones(module, num):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(num)])
+
+
 if __name__ == "__main__":
-    x = torch.ones((1, 16, 32))
-    DETR()._add_pos_embed(x)
+    x = torch.ones((1, 7, 7, 3))
+    detr = DETR(
+        1,
+        100,
+        8,
+    )
+
+    print(detr._pos_embed(x))
