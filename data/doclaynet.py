514cd08b9790cfba0782ef6224091aa65e1ae326
Trần Long <trandanghienlong2004@gmail.com>
2025-03-19 07:37:33 +0700

    feat: COCODataset
    - Support loading COCO format dataset

--- data/doclaynet.py
+++ data/doclaynet.py
@@ -1,4 +1,5 @@
 import os
+import json
 import torch
 
 from PIL import Image
@@ -75,3 +76,84 @@ class YOLODataset(Dataset):
         labels = list(labels)
         paths = list(paths)
         return images, labels, paths
+
+
+class COCODataset(Dataset):
+    def __init__(
+        self,
+        image_dir: str,
+        label_path: str,
+        num_classes=1,
+        transform=None,
+    ):
+        self.image_dir = image_dir
+        self.label_dir = image_dir
+        self.annotations = self._load_annotations(label_path)
+        self.transform = transform
+        self.num_classes = num_classes
+
+    def __len__(self):
+        return len(self.annotations["images"])
+
+    def __getitem__(self, item):
+        img_item = self.annotations["images"][item]
+        img_path = os.path.join(self.image_dir, img_item["file_name"])
+        img = Image.open(img_path)
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+
+        anns = self._get_label(img_item["id"])
+        if len(anns) == 0:
+            label = {
+                "labels": torch.as_tensor([self.num_classes], dtype=torch.int64),
+                "boxes": torch.zeros(1, 4, dtype=torch.float32),
+            }
+        else:
+            label = {
+                "labels": torch.as_tensor([box[0] for box in anns], dtype=torch.int64),
+                "boxes": torch.as_tensor(
+                    [box[1:5] for box in anns], dtype=torch.float32
+                ),
+            }
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label, img_item
+
+    def _load_annotations(self, label_path):
+        with open(label_path, "r") as f:
+            data = json.load(f)
+
+        return data
+
+    def _get_label(self, img_id):
+        labels = [
+            label
+            for label in self.annotations["annotations"]
+            if label["image_id"] == img_id
+        ]
+        boxes = []
+        for label in labels:
+            box = label["bbox"]
+            class_id = int(label["category_id"])
+            x_min = float(box[0])
+            y_min = float(box[1])
+            width = float(box[2])
+            height = float(box[3])
+
+            x_center = x_min + width / 2
+            y_center = y_min + height / 2
+            # Append the box as a tuple
+            boxes.append([class_id, x_center, y_center, width, height])
+
+        return boxes
+
+    @staticmethod
+    def collate_fn(batch):
+        images, labels, items = tuple(zip(*batch))
+
+        images = torch.stack(images, dim=0)
+        labels = list(labels)
+        items = list(items)
+        return images, labels, items
