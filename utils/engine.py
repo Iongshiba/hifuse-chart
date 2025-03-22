abf6baec0491581e6fe7ce03564c7abc38b1ede7
Trần Long <trandanghienlong2004@gmail.com>
2025-03-22 10:37:52 +0700

    fix(evaluate): major fixes
    - Label starts from 1
    - Convert to bbox to xywh before evaluate
    - Fix COCO result format
    - Calculate mean precision, recall for IoU = 0.5

--- utils/engine.py
+++ utils/engine.py
@@ -2,9 +2,11 @@ import os
 import sys
 import torch
 
+import numpy as np
+
 from PIL import Image
 from tqdm import tqdm
-from utils.misc import box_cxcywh_to_xyxy
+from utils.misc import box_cxcywh_to_xywh
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
@@ -31,7 +33,7 @@ def train_one_epoch(
         loss.backward()
         accu_loss += loss
 
-        bar.desc = f"[Train Epoch {epoch}]\tLoss: {loss.item():.3f}\tLR: {optimizer.param_groups[0]['lr']:.6f}"
+        bar.desc = f"[Train Epoch {epoch}] Loss: {loss.item():.3f}\tLR: {optimizer.param_groups[0]['lr']:.6f}"
         if not torch.isfinite(loss):
             print("WARNING: non-finite loss, ending training ", loss)
             sys.exit(1)
@@ -71,9 +73,11 @@ def evaluate(model, dataloader, criterion, device, epoch):
     out_bboxes = torch.cat(out_bboxes, dim=0)
 
     prob = out_logits.softmax(-1)
-    scores, labels = prob[..., :-1].max(-1)
+    # change the index if non_object_index changes
+    prob[..., 0] *= 0
+    scores, labels = prob.max(-1)
 
-    boxes = box_cxcywh_to_xyxy(out_bboxes)
+    boxes = box_cxcywh_to_xywh(out_bboxes)
     # and from relative [0, 1] to absolute [0, height] coordinates
     img_h, img_w = images_coco[0]["height"], images_coco[0]["width"]
     scale_fct = torch.tensor([img_w, img_h, img_w, img_h], device=out_bboxes.device)
@@ -84,9 +88,9 @@ def evaluate(model, dataloader, criterion, device, epoch):
         {
             "id": i,
             "image_id": images_coco[i]["id"],
-            "category_id": l,
-            "box": b,
-            "score": s,
+            "category_id": l.tolist(),
+            "bbox": b.tolist(),
+            "score": s.tolist(),
         }
         for i in range(len(images_coco))
         for l, b, s in zip(labels[i], boxes[i], scores[i])
@@ -98,19 +102,26 @@ def evaluate(model, dataloader, criterion, device, epoch):
 
     eval_coco.evaluate()
     eval_coco.accumulate()
-    precision = eval_coco.eval["precision"].mean()
-    recall = eval_coco.eval["recall"].mean()
+
+    precision = eval_coco.eval["precision"]
+    recall = eval_coco.eval["recall"]
+    precision_50_all = precision[0, :, :, 0, 2]  # IoU=0.5, all categories, all areas
+    avg_precision = np.mean(precision_50_all[precision_50_all > 0])  # Skip NaN values
+    # Get max recall at IoU=0.5 for all categories and areas
+    recall_50_all = recall[0, :, 0, 2]  # IoU=0.5, all categories, all areas
+    avg_recall = np.mean(recall_50_all[recall_50_all > 0])  # Skip NaN values
+
     eval_coco.summarize()
 
     stats = {
-        "precision": precision.item(),
-        "recall": recall.item(),
+        "precision": avg_precision.item(),
+        "recall": avg_recall.item(),
         "mAP50": eval_coco.stats[1].item(),
         "mAP5095": eval_coco.stats[0].item(),
     }
 
     print(
-        f"[Val Epoch {epoch}]\tPrecision: {precision.item():.2f}\tRecall: {recall.item():.2f}\tmAP@.5: {eval_coco.stats[1].item():.2f}\tmAP@[.5:.95]: {eval_coco.stats[0].item():.2f}"
+        f"[Val Epoch {epoch}]\tPrecision: {avg_precision.item()}\tRecall: {avg_recall.item()}\tmAP@.5: {eval_coco.stats[1].item()}\tmAP@[.5:.95]: {eval_coco.stats[0].item()}"
     )
 
     return stats
