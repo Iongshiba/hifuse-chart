f3ee2a8d9d46d2d064125529d8ddfb4a468b3faf
Trần Long <trandanghienlong2004@gmail.com>
2025-04-02 23:06:05 +0700

    fix: remove duplicate opimizer.step()
    - optimizer.step() is already called in scaler(optimizer)

--- utils/engine.py
+++ utils/engine.py
@@ -54,7 +54,6 @@ def train_one_epoch(
 
         if max_norm > 0:
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
-        optimizer.step()
 
         scaler.update()
         lr_scheduler.step()
