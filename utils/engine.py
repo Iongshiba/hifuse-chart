7227fc8f90267c29718fa29c82113202a1d20fc8
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-04-26 22:34:51 +0700

    feat: add evaluate_retina() function

--- utils/engine.py
+++ utils/engine.py
@@ -146,6 +146,82 @@ def evaluate(model, dataloader, device, epoch):
 
 
 @torch.no_grad()
+def evaluate_retina(model, dataloader, device, epoch):
+    model.eval()
+
+    images_coco = []
+    coco_predictions = []
+
+    global_idx = 0
+    bar = tqdm(dataloader, file=sys.stdout, desc=f"Eval Epoch {epoch}")
+    for images, _, items in bar:
+        batch_size = len(images)
+        images = [img.to(device) for img in images]
+
+        preds = model(images)
+
+        for b in range(batch_size):
+            info = items[b]
+            img_id = info["id"]
+            # img_h, img_w = info["height"], info["width"]
+            images_coco.append(info)
+
+            pred = preds[b]
+            boxes_xyxy = pred["boxes"]  # [N,4] (x1,y1,x2,y2)
+            scores = pred["scores"]  # [N]
+            labels = pred["labels"]  # [N]
+
+            if boxes_xyxy.numel() == 0:
+                continue
+
+            # Convert to COCO xywh:
+            # x = x1, y = y1, w = x2-x1, h = y2-y1
+            x1, y1, x2, y2 = boxes_xyxy.unbind(dim=1)
+            widths = (x2 - x1).clamp(min=0)
+            heights = (y2 - y1).clamp(min=0)
+            boxes_xywh = torch.stack([x1, y1, widths, heights], dim=1)
+
+            for box, score, label in zip(boxes_xywh, scores, labels):
+                coco_predictions.append(
+                    {
+                        "id": global_idx,
+                        "image_id": img_id,
+                        "category_id": int(label.item()),
+                        "bbox": box.cpu().tolist(),
+                        "score": float(score.item()),
+                    }
+                )
+                global_idx += 1
+
+    # Load ground truth and detections
+    gt_coco = COCO(dataloader.dataset.label_path)
+    dt_coco = gt_coco.loadRes(coco_predictions)
+    coco_eval = COCOeval(gt_coco, dt_coco, iouType="bbox")
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
+
+    # Compute the aggregated metrics
+    stats = coco_eval.stats
+    mAP5095 = float(stats[0])  # AP@[.5:.95]
+    mAP50 = float(stats[1])  # AP@.5
+    recall = coco_eval.eval["recall"]
+    recall50 = recall[0, :, :, 0]  # [R, K]
+    avg_recall = float(np.nanmean(recall50))
+
+    precision = coco_eval.eval["precision"]
+    precision50 = precision[0, :, :, 0, 2]
+    avg_precision = float(np.nanmean(precision50))
+
+    return {
+        "eval/precision": avg_precision,
+        "eval/recall": avg_recall,
+        "eval/mAP50": mAP50,
+        "eval/mAP5095": mAP5095,
+    }
+
+
+@torch.no_grad()
 def plot_img(model, dataset, device, args):
     model.eval()
     images = []
