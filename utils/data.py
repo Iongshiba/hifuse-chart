a00acf83bae17533ead8267841d46469b628fa8d
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-04-27 03:58:47 +0700

    fix: type error on exception raise

--- utils/data.py
+++ utils/data.py
@@ -179,7 +179,9 @@ def read_data_detection_coco(root: str):
         val_label_path = os.path.join(COCO_dir, "val.json")
         return train_images_dir, train_label_path, val_images_dir, val_label_path
     else:
-        raise ("Could not find the dataset, please check the path or the format.")
+        raise Exception(
+            "Could not find the dataset, please check the path or the format."
+        )
 
 
 class DetectionTransform:
