8c3ee311a8cde08257aa9e6d8a9e4fdc6a2a72e5
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-04-29 14:19:54 +0700

    feat: update TriFuse
    - add various attributes
    - implement _train()
    - implement _load_cfg()
    - implement _wandb_init()

--- Trifuse/models/trifuse.py
+++ Trifuse/models/trifuse.py
@@ -1,13 +1,69 @@
-# Integrate between Trifuse backbone and detection head
+from argparse import Namespace
+from typing import List, Union
+import wandb
+import os
+import yaml
+from pathlib import Path
+from Trifuse.utils import DEFAULT_CONFIG_FILE, RANK
+from Trifuse.utils.build import build_backbone, build_detect_head
+from Trifuse.utils.trainer import TriFuseTrainer
 
-import torch.nn as nn
 
+class TriFuse:
+    def __init__(
+        self,
+        head: str,
+        num_classes: int,
+        variant: str = "tiny",
+    ):
+        self.backbone = build_backbone(num_classes, variant)
+        self.head = build_detect_head(num_classes, head)
+        self.args = None
+        self.logger = None
+        self.trainer = None
 
-class TriFuse(nn.Module):
-    def __init__(self, backbone, head):
-        super(TriFuse, self).__init__()
-        self.backbone = backbone
-        self.head = head
+    def train(
+        self,
+        config_path: Union[str, Path] = DEFAULT_CONFIG_FILE,
+        **overrides,
+    ):
+        # Load config file
+        print("[INFO] Loading config file...")
+        if "config_path" in overrides:
+            config_path = overrides.pop("config_path")
+        self.args = self._load_cfg(config_path, overrides)
 
-    def forward(self, x):
-        pass
+        # Initialize Wandb
+        print("[INFO] Initializing Wandb...")
+        self._wandb_init(self.args.enable_logger)
+
+        # Initialize trainer
+        print("[INFO] Initializing trainer...")
+        self.trainer = TriFuseTrainer(self.args)
+
+        # Start training
+        print("[INFO] Starting training...")
+        self.trainer.train()
+
+        # Done training
+        print("[INFO] Training done.")
+
+    def _load_cfg(self, cfg_path: Union[str, Path], overrides: dict) -> Namespace:
+        if isinstance(cfg_path, (str, Path)):
+            with open(cfg_path, errors="ignore", encoding="utf-8") as f:
+                data = f.read()
+                cfg = yaml.safe_load(data) or {}
+        for key, value in overrides.items():
+            cfg[key] = value
+
+        cfg = Namespace(**cfg)
+        return cfg
+
+    def _wandb_init(self, enable_logger: bool):
+        if RANK == 0 and enable_logger:
+            wandb.login(key=os.environ["WANDB_API_KEY"])
+            self.logger = wandb.init(project="trifuse", config=vars(self.args))
+            self.logger.define_metric("eval/precision", summary="max")
+            self.logger.define_metric("eval/recall", summary="max")
+            self.logger.define_metric("eval/mAP50", summary="max")
+            self.logger.define_metric("eval/mAP5095", summary="max")
