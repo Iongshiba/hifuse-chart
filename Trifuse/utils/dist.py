daa6cea533016f9ccaa098531448aa8a7f4c264e
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-04-30 21:14:10 +0700

    feat: rewrite the temp script for DDP

--- Trifuse/utils/dist.py
+++ Trifuse/utils/dist.py
@@ -60,17 +60,16 @@ def generate_ddp_file(trainer):
     )
 
     content = f"""
-# Ultralytics Multi-GPU training temp file (should be automatically deleted after use)
 overrides = {vars(trainer.args)}
 
 if __name__ == "__main__":
     from {module} import {name}
-    from ultralytics.utils import DEFAULT_CFG_DICT
+    from Trifuse.utils import DEFAULT_CONFIG_FILE
+    from Trifuse.utils.misc import load_config
 
-    cfg = DEFAULT_CFG_DICT.copy()
-    cfg.update(save_dir='')   # handle the extra key 'save_dir'
-    trainer = {name}(cfg=cfg, overrides=overrides)
-    trainer.args.model = "{getattr(trainer.hub_session, "model_url", trainer.args.model)}"
+    cfg = load_config(DEFAULT_CONFIG_FILE, overrides)
+    trainer = {name}(cfg)
+    trainer.args.model = {trainer.args.model}"
     results = trainer.train()
 """
     (USER_CONFIG_DIR / "DDP").mkdir(exist_ok=True)
